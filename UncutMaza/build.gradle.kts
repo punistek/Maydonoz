0a95b3b9f2a5096285ffc6faf12de4b079d39b34
punistek <104331040+punistek@users.noreply.github.com>
2026-09-11 23:58:05 +0300

    Delete UncutMaza directory

--- UncutMaza/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 3
-
-cloudstream {
-    language = "hi"
-    description = "UncutMaza - Latest / Most Viewed / Longest / Popular / Random"
-    authors = listOf("PARS")
-    status = 1
-    tvTypes = listOf("Movie")
-}
