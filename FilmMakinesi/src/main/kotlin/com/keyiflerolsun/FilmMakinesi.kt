e2769304eb861ea66cfe53423daac816679675ec
punistek <104331040+punistek@users.noreply.github.com>
2026-09-12 00:42:35 +0300

    Update FilmMakinesi.kt

--- FilmMakinesi/src/main/kotlin/com/keyiflerolsun/FilmMakinesi.kt
+++ FilmMakinesi/src/main/kotlin/com/keyiflerolsun/FilmMakinesi.kt
@@ -120,7 +120,7 @@ class FilmMakinesi : MainAPI() {
 
             Log.d(
                 "FILMMAKINESI",
-                "CARD title=$title poster=${poster ?: "NONE"}"
+                "CARD title=$title poster=${poster ?: "NONE"} posterHost=${poster?.let { runCatching { java.net.URI(it).host }.getOrNull() }}"
             )
 
             val year = a.selectFirst(".item-footer .info span")
