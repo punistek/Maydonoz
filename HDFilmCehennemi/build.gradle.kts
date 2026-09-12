1fd8e0e3ab9b4f1360dcce30ca908eb65bba30a6
punistek <104331040+punistek@users.noreply.github.com>
2026-09-12 18:25:59 +0300

    Update authors in build.gradle.kts

--- HDFilmCehennemi/build.gradle.kts
+++ HDFilmCehennemi/build.gradle.kts
@@ -1,7 +1,7 @@
 version = 48
 
 cloudstream {
-    authors     = listOf("hexated", "keyiflerolsun")
+    authors     = listOf("PUU", "PARS")
     language    = "tr"
     description = "Türkiye'nin en hızlı hd film izleme sitesi"
 
