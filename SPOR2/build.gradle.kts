e16739b030880ea2adf1c8bff4e140c31bcafb44
punistek <104331040+punistek@users.noreply.github.com>
2026-09-12 18:18:20 +0300

    Delete SPOR2 directory

--- SPOR2/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 3
-
-cloudstream {
-    description = "SPOR2 - izlemaç match-center + /t resolver + nokta atışı tanılama logları"
-    authors = listOf("PARS")
-    language = "tr"
-    status = 1
-    tvTypes = listOf("Live")
-}
