61171fbdb6c81dec10442e6f6354b7ce8c8d48dc
punistek <104331040+punistek@users.noreply.github.com>
2026-09-12 18:23:24 +0300

    Update build.gradle.kts

--- SinemaCX/build.gradle.kts
+++ SinemaCX/build.gradle.kts
@@ -1,7 +1,7 @@
 version = 24
 
 cloudstream {
-    authors     = listOf("keyiflerolsun")
+    authors     = listOf("PARS")
     language    = "tr"
     description = "Güncel ve en iyi yabancı filmleri yüksek görüntü kalitesinde, Türkçe dublaj ve altyazı seçenekleriyle filmleri full izleyin."
 
