c1b2aa836b42671c5bb82018ec4ea41f8b3c50d0
punistek <104331040+punistek@users.noreply.github.com>
2026-09-13 00:15:59 +0300

    Update Sinemakolik.kt

--- Sinemakolik/src/main/kotlin/com/keyiflerolsun/Sinemakolik.kt
+++ Sinemakolik/src/main/kotlin/com/keyiflerolsun/Sinemakolik.kt
@@ -4,8 +4,7 @@ import android.util.Log
 import android.util.Base64 as AndroidBase64
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.loadExtractor
+import com.lagradost.cloudstream3.utils.*
 import org.jsoup.Jsoup
 import org.json.JSONObject
 import java.net.URLDecoder
