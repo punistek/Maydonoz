b7dbdbaba64a33e2d43afd21a3c462967eec54d3
punistek <104331040+punistek@users.noreply.github.com>
2026-09-12 20:59:21 +0300

    Delete SPOR2 directory

--- SPOR2/src/main/kotlin/com/spor2/SPOR2Plugin.kt
+++
@@ -1,8 +0,0 @@
-package com.spor2
-import android.content.Context
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-@CloudstreamPlugin
-class SPOR2Plugin : Plugin() {
-    override fun load(context: Context) { registerMainAPI(SPOR2()) }
-}
