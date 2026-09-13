b090438bbd5a5c1d6555d9ac9c36fd69bff26179
punistek <104331040+punistek@users.noreply.github.com>
2026-09-13 22:15:32 +0300

    Add files via upload

---
+++ RoketDizi/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "tr"
+    description = "RoketDizi film, dizi ve trend kaynağı"
+    authors = listOf("PARS")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://roketdizi.life/assets/favicon/apple-touch-icon.png"
+}
---
+++ RoketDizi/src/main/kotlin/com/pars/roketdizi/RoketDiziPlugin.kt
@@ -0,0 +1,12 @@
+package com.pars.roketdizi
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class RoketDiziPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(RoketDizi())
+    }
+}
