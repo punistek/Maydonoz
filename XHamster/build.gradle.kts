7babc0b26c59826f82c1b9beb0e9791b5b8e143b
punistek <104331040+punistek@users.noreply.github.com>
2026-09-14 10:15:48 +0300

    Update build.gradle.kts

--- XHamster/build.gradle.kts
+++ XHamster/build.gradle.kts
@@ -1,10 +1,9 @@
 version = 1
 
 cloudstream {
-    description = "XHamster video provider"
+    description = "XHamster +18 VPN ile açın."
     authors = listOf("PARS")
     status = 1
     tvTypes = listOf("Movie")
     iconUrl = "https://www.google.com/s2/favicons?domain=xhamster.com&sz=%size%"
-    isCrossPlatform = true
 }
