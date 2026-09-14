1e5cb59d953a8f207d957c2ea3166b7d225f5171
punistek <104331040+punistek@users.noreply.github.com>
2026-09-14 18:22:28 +0300

    Delete MahsunSports directory

--- MahsunSports/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 5
-cloudstream {
-    iconUrl = "https://raw.githubusercontent.com/punistek/pars-plugins/main/assets/providers/MahsunSports.jpg"
-    description = "🟢 TurkSpor kaynağından · PARS"
-    language = "tr"
-    status = 3
-    tvTypes = listOf("Live")
-    requiresResources = false
-}
