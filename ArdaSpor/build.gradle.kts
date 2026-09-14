a10c536b7f261d1361f2bd33d73483194d5e15c2
punistek <104331040+punistek@users.noreply.github.com>
2026-09-14 18:22:12 +0300

    Delete ArdaSpor directory

--- ArdaSpor/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 6
-cloudstream {
-    iconUrl = "https://raw.githubusercontent.com/punistek/pars-plugins/main/assets/providers/ArdaSpor.png"
-    description = "🟢 TurkSpor kaynağından · PARS"
-    language = "tr"
-    status = 3
-    tvTypes = listOf("Live")
-    requiresResources = false
-}
